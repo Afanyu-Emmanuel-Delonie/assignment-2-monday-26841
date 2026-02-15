b7ab3997143e438e2aa29f1377b2e6e72ccc5b58
afanyu <afanyuemma2002@gmail.com>
2026-02-15 23:41:29 +0200

    correction in the file structure

--- question2-student-api/.history/src/main/java/com/afanyu/question2_student_api/controller/StudentController_20260215234053.java
+++ question2-student-api/.history/src/main/java/com/afanyu/question2_student_api/controller/StudentController_20260215234053.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.afanyu.question2_student_api.models.Student;
+import com.afanyu.question2_student_api.models.student.Student;
 
 @RestController
 @RequestMapping("/api/students")
